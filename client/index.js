7afe24c460492e5325e26b1a8a1c480f2ac0ab3d
nelsonjlgedeon <nelson.gedeon@gmail.com>
2019-05-24 22:30:37 -0400

    Setup Local Git Workflow

--- client/index.js
+++ client/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<hr />, document.getElementById('root'));
+ReactDOM.render(
+  <div>Will Beging Adding Components</div>,
+  document.getElementById('root')
+);
