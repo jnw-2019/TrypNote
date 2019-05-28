6617b3dfe1dda5372353aa40569da9e428ba22ae
Juan Torres <jet@Juans-MBP.home>
2019-05-28 00:38:21 -0400

    models set up

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 
 router.use('/api/entries', require('./Entry'));
+router.use('/api/locations', require('./Location'));
+router.use('/api/weathers', require('./Weather'));
+router.use('/api/categories', require('./Category'));
 
 module.exports = router;
