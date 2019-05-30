bc5ebd5e824e7cdc168ba3bdb65ef4d740ea427b
William Griffin <w.griffin13@gmail.com>
2019-05-29 21:41:37 -0400

    rebuilt login page using material UI. Started new entry seed file

--- client/Login.js
+++ client/Login.js
@@ -1,29 +1,88 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+
 
 class Login extends Component {
 
     render () {
+
         return (
-            <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col">
-                        <div className="card text-center mt-5 w-50 mx-auto">
-                            <div className="card-body">
-                                <h2>TrypNote</h2>
-                                <form>
-                                    <div className="form-group">
-                                        <input type="email" className="form-control" name="email" placeholder="Email" />
-                                    </div>
-                                    <div className="form-group">
-                                        <input type="password" className="form-control" name="password" placeholder="Password" />
-                                    </div>
-                                    <button type="submit" className="btn btn-primary">Submit</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box mt={8}>
+                    <Grid
+                        container
+                        direction="row"
+                        justify="center"
+                        alignItems="center"
+                    >
+                        <Typography component="h1" variant="h2">
+                            TrypNote
+                        </Typography>
+                        <form noValidate>
+                            <TextField
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                            />
+                            <TextField
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                            />
+                            <FormControlLabel
+                                control={<Checkbox value="remember" color="primary" />}
+                                label="Remember me"
+                            />
+                            <Box
+                                mb={1}
+                            >
+                                <Button
+                                    type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    color="primary"
+                                >
+                                    Sign In
+                                </Button>
+                            </Box>
+                            <Grid container>
+                                <Grid item xs>
+                                    <Link href="#" variant="body2">
+                                        Help...password?
+                                    </Link>
+                                </Grid>
+                                <Grid item>
+                                    <Link href="#" variant="body2">
+                                        <Box fontWeight={900}>Join today!</Box>
+                                    </Link>
+                                </Grid>
+                            </Grid>
+                        </form>
+                    </Grid>
+                </Box>
+            </Container>
         )
     }
 }
---
+++ server/db/entrySeed.js
@@ -0,0 +1,18 @@
+export const entrySeed = [
+    {
+        title: 'To Kill a Mockingbird',
+        text: 'Atticus said to Jem one day, “I’d rather you shot at tin cans in the backyard, but I know you’ll go after birds. Shoot all the blue jays you want, if you can hit ‘em, but remember it’s a sin to kill a mockingbird.” That was the only time I ever heard Atticus say it was a sin to do something, and I asked Miss Maudie about it. “Your father’s right,” she said. “Mockingbirds don’t do one thing except make music for us to enjoy. They don’t eat up people’s gardens, don’t nest in corn cribs, they don’t do one thing but sing their hearts out for us. That’s why it’s a sin to kill a mockingbird.',
+    },
+    {
+        title: 'The Bell Jar',
+        text: 'I took a deep breath and listened to the old brag of my heart. I am, I am, I am.'
+    },
+    {
+        title: 'The Little Prince',
+        text: 'The most beautiful things in the world cannot be seen or touched, they are felt with the heart.'
+    },
+    {
+        title: 'God Bless You, Mr. Rosewarer',
+        text: 'Hello babies. Welcome to Earth. It’s hot in the summer and cold in the winter. It’s round and wet and crowded. On the outside, babies, you’ve got a hundred years here. There’s only one rule that I know of, babies-“God damn it, you’ve got to be kind.'
+    }
+];
