787ca398a5d2230fa7388b2f42daeed6a276785b
William Griffin <w.griffin13@gmail.com>
2019-06-02 13:54:15 -0400

    added additional seed entries with real data

--- client/Login.js
+++ client/Login.js
@@ -10,7 +10,6 @@ import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
 
-
 class Login extends Component {
 
     render () {
