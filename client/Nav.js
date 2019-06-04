d683e9fa846b1ae78528cd1e3ad3ed3133b854f8
nelsonjlgedeon <nelson.gedeon@gmail.com>
2019-06-03 20:16:48 -0400

    Merged login with home screen including Mapbox and navbar

--- client/Nav.js
+++ client/Nav.js
@@ -153,8 +153,8 @@ const Nav = () => {
               open={openAccountMenu}
               onClose={handleClose}
             >
-              <MenuItem onClick={handleClose}>Profile</MenuItem>
               <MenuItem onClick={handleClose}>My account</MenuItem>
+              <MenuItem onClick={handleClose}>Logout</MenuItem>
             </Menu>
           </div>
         </Toolbar>
