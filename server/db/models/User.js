01d470b200a37bb05cf180fae225769c6eec5fc0
William Griffin <w.griffin13@gmail.com>
2019-06-09 11:49:10 -0400

    removed return password from User model by default

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -1,40 +1,48 @@
 const conn = require('../db');
 const Sequelize = require('sequelize');
 
-const User = conn.define('user', {
-    uuid: {
-        type: Sequelize.UUID,
-        allowNull: false,
-        defaultValue: Sequelize.UUIDV4
+const User = conn.define('user',
+    {
+        uuid: {
+            type: Sequelize.UUID,
+            allowNull: false,
+            defaultValue: Sequelize.UUIDV4
+        },
+        firstname: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        lastname: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        phonenumber: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        email: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: true
+        }
+        },
+        password: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        admin: {
+            type: Sequelize.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
     },
-    firstname: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    lastname: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    phonenumber: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    email: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        validate: {
-            isEmail: true
-       }
-    },
-    password: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    admin: {
-        type: Sequelize.BOOLEAN,
-        allowNull: false,
-        defaultValue: false
-    }
-})
+    {
+        defaultScope: {
+            attributes: {
+            exclude: ['password']
+            }
+        },
+    })
 
 module.exports = User;
