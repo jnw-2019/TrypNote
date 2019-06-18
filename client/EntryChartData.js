70eaf55597a6cc42b193cc8410bc8193ea87f2af
nelsonjlgedeon <nelson.gedeon@gmail.com>
2019-06-18 13:39:08 -0400

    Weather, Time charts are rendering on dashboard now. Chart type filters displayed

--- client/EntryChartData.js
+++ client/EntryChartData.js
@@ -3,6 +3,7 @@ import { Bar, Line } from 'react-chartjs-2';
 import { connect } from 'react-redux';
 import axios from 'axios';
 import dateFormat from 'dateformat';
+import EntryChartChips from './EntryChartChips';
 
 const mapStateToProps = ({ user }) => {
   return { user };
@@ -13,7 +14,7 @@ class EntryChartData extends Component {
     super(props);
     this.state = {
       entries: [],
-      chartType: '',
+      chartType: 'Weather',
     };
   }
   componentDidMount() {
@@ -33,7 +34,7 @@ class EntryChartData extends Component {
       .then(userData => this.setState({ entries: userData.entries }));
   };
   render() {
-    const { entries } = this.state;
+    const { entries, chartType } = this.state;
     const distinctWeatherConditions = entries.reduce((acc, entry) => {
       if (
         acc[
@@ -53,8 +54,15 @@ class EntryChartData extends Component {
       }
       return acc;
     }, {});
-    console.log(distinctWeatherConditions);
-    const data = {
+    const distinctHours = entries.reduce((acc, entry) => {
+      if (acc[dateFormat(entry.createdAt, 'H')]) {
+        acc[dateFormat(entry.createdAt, 'H')] += 1;
+      } else {
+        acc[dateFormat(entry.createdAt, 'H')] = 1;
+      }
+      return acc;
+    }, {});
+    const barData = {
       labels: Object.keys(distinctWeatherConditions).sort(),
       datasets: [
         {
@@ -73,7 +81,55 @@ class EntryChartData extends Component {
         },
       ],
     };
-    return <Bar data={data} />;
+    const lineData = {
+      labels: Object.keys(distinctHours).sort((a, b) => {
+        return parseInt(a) - parseInt(b);
+      }),
+      datasets: [
+        {
+          label: 'Entries',
+          fill: true,
+          lineTension: 0.1,
+          backgroundColor: 'rgba(75,192,192,0.4)',
+          borderColor: 'rgba(75,192,192,1)',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: Object.keys(distinctHours)
+            .sort((a, b) => {
+              return parseInt(a) - parseInt(b);
+            })
+            .reduce((acc, hour) => {
+              acc.push(distinctHours[hour]);
+              return acc;
+            }, []),
+        },
+      ],
+    };
+    const selectChart = type => {
+      this.setState({ chartType: type });
+    };
+    return (
+      <div>
+        <EntryChartChips
+          chartTypes={[{ name: 'Weather' }, { name: 'Time' }, { name: 'Mood' }]}
+          selectChart={selectChart}
+        />
+        {chartType === 'Weather' ? <Bar data={barData} /> : ''}
+        {chartType === 'Time' ? <Line data={lineData} /> : ''}
+        {chartType === 'Mood' ? 'Create Pie Chart' : ''}
+      </div>
+    );
   }
 }
 
